fc36c76f8d7ea2cabef6c078a3f6572b04fe1b8d
Jacob Boyce <jboyce313@gmail.com>
2023-05-10 15:36:27 -0500

    changed auth.js and server.js to work with graphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,31 +1,26 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  authMiddleware: function ({ req, res, next }) {
+    // allows token to be sent via headers
+    const token = req.headers.authorization || "";
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return res.status(400).json({ message: "You have no token!" });
     }
 
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    } catch (err) {
+      console.log("Invalid token:", err.message);
+      return res.status(400).json({ message: "Invalid token!" });
     }
 
     // send to next endpoint
