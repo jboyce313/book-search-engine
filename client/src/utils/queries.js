6e0cf97057812517502c95d8c1f883e4cb307c1e
Jacob Boyce <jboyce313@gmail.com>
2023-05-11 07:53:01 -0500

    wrote queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query getMe($profileId: ID!) {
+    me(profileId: $profileId) {
+      _id
+      username
+      email
+      password
+      saveBooks
+    }
+  }
+`;
